57e4edaab6f0761aa6f8fdeec3d42146f5b7529b
Nick Vandewiele <nickvandewiele@gmail.com>
2015-09-16 13:09:06 -0400

    ensure that makeNewSpecies always accepts a Species object

--- rmgpy/rmg/main.py
+++ rmgpy/rmg/main.py
@@ -425,8 +425,8 @@ class RMG(util.Subject):
                 
             # Also always add in a few bath gases (since RMG-Java does)
             for label, smiles in [('Ar','[Ar]'), ('He','[He]'), ('Ne','[Ne]'), ('N2','N#N')]:
-                molecule = Molecule().fromSMILES(smiles)
-                spec, isNew = self.reactionModel.makeNewSpecies(molecule, label=label, reactive=False)
+                spec = Species().fromSMILES(smiles)
+                spec, isNew = self.reactionModel.makeNewSpecies(spec, label=label, reactive=False)
                 if isNew:
                     self.initialSpecies.append(spec)
             
